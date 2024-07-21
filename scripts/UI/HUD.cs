963262c4af932dda92aed199f1fa7ed1ef73aaea
Parker King <gnikrekrap@gmail.com>
2024-07-21 01:56:19 -0600

    Add animation when rage counter appears

--- scripts/UI/HUD.cs
+++ scripts/UI/HUD.cs
@@ -7,10 +7,13 @@ public class HUD : CanvasLayer
 
     private GameManager _gameManager;
     private Label _rageLabel;
+    private bool _wasHidden = true;
 
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
     {
+        _wasHidden = true;
+
         _gameManager = GetNode<GameManager>("/root/GameManager");
         _gameManager.Connect("RageUpdated", this, "UpdateRage");
 
@@ -25,10 +28,16 @@ public class HUD : CanvasLayer
         if (rage > 0)
         {
             _rageLabel.Show();
+            if (_wasHidden)
+            {
+                _wasHidden = false;
+                GetNode<AnimationPlayer>("EnemyRage/AppearAnimation").Play("appear");
+            }
         }
         else
         {
             _rageLabel.Hide();
+            _wasHidden = true;
         }
     }
 }
